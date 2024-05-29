86c9eddea37a498c9f38a2fcdfb8f372508b01d9
Esther <esther.zirkler@gmail.com>
2024-05-29 11:33:42 +0200

    02 / 1. + 2. Iteration Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,12 +4,38 @@ public class App {
     
     public static void main(String[] args) {
      
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+        Triangle triangle = new Triangle(10.0,10.0);
+        
+        // Das geht jetzt nicht mehr... Abstraktion
+        // Shape shape = new Shape();
+            
+        output("------ Flächen ------"); 
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Kreis: " + circle.area());
+        output("Fläche Dreieck: " + triangle.area());
+
+        output("------  Differenzen ------");
+        output("Diff. r-t :" + areaDiff(rectangle, triangle));
+
+        output("Diff. r-c :" + areaDiff(rectangle, circle));
+
        
     }
 
-  
+    // 1. Iteration: ggf. 1000x überladen
+    // private static double areaDiff(Rectangle r, Triangle t){
+    //     return r.area() - t.area();
 
-   
+    // }
+
+
+    // 2. Iteration: Polymorphie - besser
+    private static double areaDiff(Shape s1, Shape s2){
+        return s1.area() - s2.area();
+
+    }
 
 
     private static void output(String outputStr) {
